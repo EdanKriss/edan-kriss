ba71698c99772e14be5b5e79877903bc228bb7d7
Edan Kriss <maedreth@gmail.com>
2019-12-11 17:42:48 -0600

    tweaked animation delays

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default (props) => {
-    const animationClass = (props.index % 2 === 0) ? "slide-in-left-1s" : "slide-in-right-1s"
+    const animationClass = (props.index % 2 === 0) ? "slide-in-left-1s" : "slide-in-right-1s";
+    const animationDelay = `${props.index*0.3}s`;
 
     return (
-        <div className={`project shadow-large ${animationClass}`}>
+        <div className={`project shadow-large ${animationClass}`} style={{animationDelay:animationDelay}}>
             <div className="project-image">
                 <img src={props.projectImg} />
             </div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -6,47 +6,47 @@ export default (props) => {
             <div style={{height: "6rem"}}></div>
             <h2 className="heading">Core Technologies</h2>
             <ul>
-                <li className="flip-in-x-dot8s">JavaScript</li>
-                <li className="flip-in-x-dot8s">React</li>
-                <li className="flip-in-x-dot8s">Bootstrap</li>
-                <li className="flip-in-x-dot8s">Webpack</li>
-                <li className="flip-in-x-dot8s">NGINX</li>
-                <li className="flip-in-x-dot8s">Node.js</li>
-                <li className="flip-in-x-dot8s">Express</li>
-                <li className="flip-in-x-dot8s">Tomcat/Jetty</li>
-                <li className="flip-in-x-dot8s">Heroku/AWS</li>
-                <li className="flip-in-x-dot8s">VirtualBox</li>
-                <li className="flip-in-x-dot8s">Git</li>
-                <li className="flip-in-x-dot8s">Bash</li>
-                <li className="flip-in-x-dot8s">Linux</li>
-                <li className="flip-in-x-dot8s">Active Directory</li>
-                <li className="flip-in-x-dot8s">AWS</li>
+                <li className="flip-in-x-1dot5s">JavaScript</li>
+                <li className="flip-in-x-1dot5s">React</li>
+                <li className="flip-in-x-1dot5s">Bootstrap</li>
+                <li className="flip-in-x-1dot5s">Webpack</li>
+                <li className="flip-in-x-1dot5s">NGINX</li>
+                <li className="flip-in-x-1dot5s">Node.js</li>
+                <li className="flip-in-x-1dot5s">Express</li>
+                <li className="flip-in-x-1dot5s">Tomcat/Jetty</li>
+                <li className="flip-in-x-1dot5s">Heroku/AWS</li>
+                <li className="flip-in-x-1dot5s">VirtualBox</li>
+                <li className="flip-in-x-1dot5s">Git</li>
+                <li className="flip-in-x-1dot5s">Bash</li>
+                <li className="flip-in-x-1dot5s">Linux</li>
+                <li className="flip-in-x-1dot5s">Active Directory</li>
+                <li className="flip-in-x-1dot5s">AWS</li>
             </ul>
             <br/><br/>
             <h2 className="heading">Protocols</h2>
             <ul>
-                <li className="flip-in-x-1dot5s">TCP</li>
-                <li className="flip-in-x-1dot5s">UDP</li>
-                <li className="flip-in-x-1dot5s">SSH</li>
-                <li className="flip-in-x-1dot5s">TLS/SSL</li>
-                <li className="flip-in-x-1dot5s">HL7</li>
-                <li className="flip-in-x-1dot5s">XMPP</li>
-                <li className="flip-in-x-1dot5s">WebSockets</li>
-                <li className="flip-in-x-1dot5s">TELNET</li>
-                <li className="flip-in-x-1dot5s">IMAP</li>
-                <li className="flip-in-x-1dot5s">SMTP</li>
-                <li className="flip-in-x-1dot5s">RDP</li>
-                <li className="flip-in-x-1dot5s">LDAP</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>TCP</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>UDP</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>SSH</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>TLS/SSL</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>HL7</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>XMPP</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>WebSockets</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>TELNET</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>IMAP</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>SMTP</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>RDP</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.4s"}}>LDAP</li>
             </ul>
             <br/><br/>
             <h2 className="heading">Some Exposure</h2>
             <ul>
-                <li className="flip-in-x-2s">Android Studio</li>
-                <li className="flip-in-x-2s">Java</li>
-                <li className="flip-in-x-2s">Ruby on Rails</li>
-                <li className="flip-in-x-2s">MySQL</li>
-                <li className="flip-in-x-2s">MongoDB</li>
-                <li className="flip-in-x-2s">Python</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.9s"}}>Android Studio</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.9s"}}>Java</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.9s"}}>Ruby on Rails</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.9s"}}>MySQL</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.9s"}}>MongoDB</li>
+                <li className="flip-in-x-1dot5s" style={{animationDelay: "0.9s"}}>Python</li>
             </ul>
         </div>
     );
