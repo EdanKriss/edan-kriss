aba4e40e5a675855b73cf25f342b65adb0a2d399
Edan Kriss <maedreth@gmail.com>
2019-12-13 13:23:02 -0600

    bundled fonts rather than load from cdn

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => {
     const animationClass = (props.index % 2 === 0) ? "slide-in-left-1s" : "slide-in-right-1s";
-    const animationDelay = `${props.index*0.3}s`;
+    const animationDelay = `${props.index*0.3+0.5}s`;
 
     return (
         <div className={`project shadow-large ${animationClass}`} style={{animationDelay:animationDelay}}>
