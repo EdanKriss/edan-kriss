b214aa2689ce0c664e91867f3b97bfbc729d4c5f
Edan Kriss <maedreth@gmail.com>
2020-02-01 02:13:33 -0600

    added project hover effect

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
+import ViewProjectCircle from '../../public/images/projects/view_project_circle.png';
+
 export default (props) => {
     const animationClass = (props.index % 2 === 0) ? "slide-in-left-1s" : "slide-in-right-1s";
     const animationDelay = `${props.index*0.3+0.5}s`;
 
     return (
-        <div className={`project shadow-large ${animationClass}`} style={{animationDelay:animationDelay}}>
+        <div 
+            className={`project shadow-large ${animationClass}`} 
+            style={{animationDelay:animationDelay}}
+            onClick={()=>{window.open(props.projectLink)}}
+        >
+            <div className="project-overlay"></div>
+            <img className="view-project-circle" src={ViewProjectCircle} />
             <div className="project-image">
                 <img src={props.projectImg} />
             </div>
             <div className="project-info">
                 <h3>{props.projectName}</h3>
                 <p>{props.projectInfo}</p>
-                <span 
+                {/* <span 
                     className="linkNewTab"
                     onClick={()=>{window.open(props.projectLink)}}
-                >View Project</span>
+                >View Project</span> */}
             </div>
         </div>
     );
