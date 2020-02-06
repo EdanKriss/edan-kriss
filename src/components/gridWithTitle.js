320d6bad051ab2d3ad8befef460eefbec6da866d
Edan Kriss <maedreth@gmail.com>
2020-02-05 23:55:07 -0600

    added tiny recipe modal text

--- src/components/gridWithTitle.js
+++ src/components/gridWithTitle.js
@@ -11,42 +11,54 @@ const rootStyle = (
     height: "58vh!important",
     width: "29vh!important"
 } : {
-    height: "76vw!important",
-    width: "38vw!important"
+    height: "152vw!important",
+    width: "76vw!important"
 };
 const Tile = withStyles({
     root: rootStyle
 })(GridListTile);
+const TileBar = withStyles({
+    root: {
+        backgroundColor: "rgba(0,0,0,0.8)",
+        fontWeight: 900
+    },
+    title: {
+        fontSize: "1.33rem"
+    },
+    subtitle: {
+        fontSize: "1rem"
+    }
+})(GridListTileBar);
 
 const tileData = [
     {
       img: require('Images/projects/tiny-recipes/login.png'),
-      title: 'Gallery 1',
-      author: 'author',
+      title: 'Login Page',
+      text: 'Passwords are hashed and stored in MongoDB',
       cols: .66,
     },
     {
       img: require('Images/projects/tiny-recipes/recipe_list.png'),
-      title: 'Gallery 2',
-      author: 'author',
+      title: 'Recipe List',
+      text: 'Our data model revolves around recipes!',
       cols: .66,
     },
     {
       img: require('Images/projects/tiny-recipes/device_permission.png'),
-      title: 'Gallery 3',
-      author: 'author',
+      title: 'Platform-agnostic code',
+      text: 'Flutter widgets dont care what the device is',
       cols: .67,
     },
     {
       img: require('Images/projects/tiny-recipes/photo_picker.png'),
-      title: 'Gallery 4',
-      author: 'author',
+      title: 'Image picker',
+      text: 'High level API for common tasks',
       cols: .66,
     },
     {
       img: require('Images/projects/tiny-recipes/test_compression.png'),
-      title: 'Testing compression libraries',
-      author: 'The raw photos taken by newer cameras were far too large for the Flutter image cache, so I tested various libraries. I managed to achieve compression by a factor of 10 without much noticible loss.',
+      title: 'Testing compression',
+      text: 'Notice the poor color of some libraries.',
       cols: .66,
     }
   ];
@@ -56,12 +68,14 @@ function ImageGridList(props) {
 		<div>
 				<GridList >
 					{tileData.map(tile => (
-						<Tile key={tile.img}  cols={(window.innerWidth > 750) ? 0.66 : 1 } >
+						<Tile key={tile.img}>
+						{/* <Tile key={tile.img}  cols={(window.innerWidth > 750) ? 0.66 : 1 } > */}
 						{/* <GridListTile key={tile.img}  cols={tile.cols}> */}
 							<img className="tiny-recipes-screenshot" src={tile.img} alt={tile.title} />
-                            <GridListTileBar 
+                            {/* <GridListTileBar  */}
+                            <TileBar 
                                 title={tile.title} 
-                                subtitle={<span>by: {tile.author}</span>}
+                                subtitle={<span>{tile.text}</span>}
                             />
 						</Tile>
 						// </GridListTile>
--- src/components/modals/tinyRecipesModal.js
+++ src/components/modals/tinyRecipesModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Modal } from 'antd';
+import Typography from '@material-ui/core/Typography';
 
 import GridWithTitles from '../gridWithTitle.js';
 
@@ -12,8 +13,33 @@ export default (props) => {
             visible={props.visible}
             onCancel={props.toggleVisibility}
             width={"90vw"}
+            bodyStyle={{
+                textAlign: "center"
+            }}
         >
             <GridWithTitles />
+
+            <div>
+                <span>
+                    <h1>Technologies:</h1>
+                    <ul style={{width:"fit-content",margin:"auto"}}>
+                        <li>Flutter</li>
+                        <li>Node JS</li>
+                        <li>Mongo DB</li>
+                        <li>AWS S3</li>
+                    </ul>
+                </span>
+
+                <span>
+                    <h1>Features:</h1>
+                    <ul style={{width:"fit-content",margin:"auto"}}>
+                        <li>Cross-platform (iOS and Android)</li>
+                        <li>Platform-aware cache management</li>
+                        <li>Distrubuted computing techniques</li>
+                        <li>Custom session management</li>
+                    </ul>
+                </span>
+            </div>
         </Modal>
     );
 }
